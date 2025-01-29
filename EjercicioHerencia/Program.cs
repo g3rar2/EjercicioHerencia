70f6bac308118eb00c9473cfa1dafb8261df63a9
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-29 14:59:00 -0600

    cuadrado1 implementation

--- EjercicioHerencia/Program.cs
+++ EjercicioHerencia/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+using EjercicioHerencia;
+
+Cuadrado cuadrado1 = new Cuadrado();
+
+Console.WriteLine("Escriba la longitud de uno de los lados del cuadrado:");
+cuadrado1.LadoCuadrado = Convert.ToInt32(Console.ReadLine());
+
+cuadrado1.Imprimir();
