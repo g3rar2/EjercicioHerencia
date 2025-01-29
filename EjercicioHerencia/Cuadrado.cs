f84e61e69c27bdf4e37d8792e7330b6ee364d435
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-29 14:47:01 -0600

    Added Cuadrado

---
+++ EjercicioHerencia/Cuadrado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EjercicioHerencia
+{
+    public class Cuadrado:Operaciones
+    {
+        public double LadoCuadrado { get; set; }
+
+        public double PerimetroCuadrado { get; set; }
+
+        public double AreaCuadrado { get; set; }
+
+        public double VolumenCuadrado { get; set; }
+
+        public void Imprimir()
+        {
+
+            Console.WriteLine($"Perimetro de Cuadrado: {Longitud(LadoCuadrado)}");
+            Console.WriteLine($"Area de Cuadrado: {Area(LadoCuadrado)}");
+            Console.WriteLine($"Area de Cuadrado: {Volumen(LadoCuadrado)}");
+
+
+        }
+    }
+}
---
+++ EjercicioHerencia/Operaciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioHerencia
+{
+    public class Operaciones
+    {
+
+        public double Longitud(double dimension)
+        {
+            return 4*dimension;
+        }
+
+        public double Area(double dimension)
+        {
+            return dimension * dimension;
+        }
+
+        public double Volumen(double dimension)
+        {
+            return dimension * dimension * dimension;
+        }
+
+    }
+}
